a0386e030c3c762de5200d324af7948bb2ed8a1b
Mohd Zain <zainmohd1998@gmail.com>
2024-01-13 01:58:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+( () =>{
+    sentence = "Hello! My Name is Zain"
+    let line = document.getElementById('line')
+    const characters = sentence.split('');
+    characters.forEach((char, index) => {
+        setTimeout(() => {
+        line.innerHTML = line.innerHTML + `${char}`
+        if (index === characters.length - 1) {
+            console.log('\n');
+        }
+        }, 80 * index);
+    });
+}
+)()
+
 let upd = document.getElementById("updatev")
 let updh = document.getElementById("updateh")
 let p = document.createElement("p")
@@ -12,3 +27,6 @@ let p2Text = document.createTextNode("Updated on : 12 Jan, 2024 Version 1.9")
 p2.append(p2Text)
 p2.setAttribute('class','visible-xs')
 updh.appendChild(p2)
+
+
+
