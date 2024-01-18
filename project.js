84b550063ee2f61fed1ff3bf507ebfe1aef96a24
Mohd Zain <zainmohd1998@gmail.com>
2024-01-18 19:45:04 +0530

    commit

---
+++ project.js
@@ -0,0 +1,159 @@
+const projects = [
+    {
+        "id" : "noteswift",
+        "title":"NoteSwift",
+        "area":"Web Development Using MongoDB, Express.js, React, Node.js",
+        "Description":"Developed a full stack web app to save the notes of users on the web which can be accessed from anywhere. Implemented secure user authentication through JWT, ensuring data integrity and user privacy.",
+        "status":"",
+        "links":{
+            details:"",
+            live:"",
+            github:"https://github.com/mohdzain98/noteswift"
+        }
+    },
+    {
+        "id" : "newslookup",
+        "title":"NewsLookup",
+        "area":"Web Development Using React",
+        "Description":"Developed a React-based news web application that retrieves news from a News API and displays it in many categories, including sports, health, science, and top stories.",
+        "status":"",
+        "links":{
+            details:"",
+            live:"",
+            github:"https://github.com/mohdzain98/newslookup"
+        }
+    },
+    {
+        "id" : "techbooks",
+        "title":"Techbooks Website",
+        "area":"Web Development Using HTML, CSS, Java Server Pages (JSP), MySQL",
+        "Description":"Simple book website that lists the recommended books for all subjects in the engineering disciplines CSE, ECE, EE, MECH, and CIVIL.<br>It will also include links to buy the respective book from Amazon and Flipcart.",
+        "status":"AWS",
+        "links":{
+            details:"",
+            live:"http://13.234.222.68:8080/techbook/",
+            github:"https://github.com/mohdzain98/techbooks"
+        }
+    },
+    {
+        "id" : "bracketsbay",
+        "title":"Curtain Brackets E-Commerce Business Website",
+        "area":"Web Development Using HTML, CSS, PHP, MySQL",
+        "Description":"I have conceptualized, designed, and launched an e-commerce website specializing in curtain brackets. This online platform showcases an extensive range of premium curtain bracket solutions, catering to diverse customer needs and design preferences"
+        +"The website combines sleek design with user-friendly navigation, ensuring a seamless shopping experience for visitors",
+        "status":"000webhost by Hostinger",
+        "links":{
+            details:"",
+            live:"https://bracktesbay.000webhostapp.com/",
+            github:"https://github.com/mohdzain98/bracketsbay"
+        }
+    },
+    {
+        "id" : "akenltk",
+        "title":"Automated Keyword Extraction from Articles",
+        "area":"Information Retrieval using NLP",
+        "Description":"In research & news articles, keywords form an important component since they provide a concise representation of the article's content. Keywords also play a crucial role in locating the article from information retrieval systems, bibliographic databases and for search engine optimization. Keywords also help to categorize the article into the relevant subject or discipline.<br/>"
+        +"In this Project, we will be extracting keywords from a dataset that contains about 3,800 abstracts. The original dataset is from Kaggle — NIPS Paper.",
+        "status":"",
+        "links":{
+            details:"",
+            live:"",
+            github:"https://github.com/mohdzain98/Automated_Keyword_Extraction"
+        }
+    },
+    {
+        "id" : "scd",
+        "title":"Skin Cancer Detection",
+        "area":"Health Analytics using Convolutional Neural Network CNN",
+        "Description":"Skin cancer is the most common human malignancy, is primarily diagnosed visually, beginning with an initial clinical screening and followed potentially by dermoscopic analysis, a biopsy and histopathological examination. Automated classification of skin lesions using images is a challenging task owing to the fine-grained variability in the appearance of skin lesions.",
+        "status":"",
+        "links":{
+            details:"projects/skin_cancer_detection.html",
+            live:"",
+            github:"https://github.com/mohdzain98/Skin_Cancer_Detection"
+        }
+    },
+    {
+        "id" : "app",
+        "title":"Air Pollution Prediction",
+        "area":"Time Series using Recurrent Neural Network RNN",
+        "Description":"Over the past few decades, due to human activities, industrialization, and urbanization, air pollution has become a life-threatening factor in many countries around the world. Among air pollutants, Particulate Matter with a diameter of less than 2.5μm (PM2.5) is a serious health problem. This project aims to study the pollutant in the air of a city and try to predicts the pollution.",
+        "status":"",
+        "links":{
+            details:"projects/air_pollution_prediction.html",
+            live:"",
+            github:"https://github.com/mohdzain98//Air_Pollution_Prediction"
+        }
+    },
+    {
+        "id" : "fpmvp",
+        "title":"Football Player's Market Value Prediction",
+        "area":"Sports Analytics using Artificial Neural Network ANN",
+        "Description":"In recent years, neural networks and deep learning have become popular methods for predicting the market value of football players.<br> In this project Neural Network was implemented on a large dataset of player's value of year 2021 that was used to train models that can accurately predict the value of a player based on various factors such as age, height, weight, stamina, strength, skill moves, work rate, performance, team etc. <br> With the help of this method, football clubs can make better decisions when it comes to buying and selling players.",
+        "status":"",
+        "links":{
+            details:"projects/fpmvp.html",
+            live:"",
+            github:"https://github.com/mohdzain98/Football-Player-s-Market_value_Prediction"
+        }
+    },
+    {
+        "id" : "rpo",
+        "title":"Retail Price Optimization",
+        "area":"Business Analytics using Artificial Neural Network ANN",
+        "Description":"Price optimization is using historical data to identify the most appropriate price of a product or a service that maximizes the company's profitability. There are numerous factors like demography, operating costs, survey data, etc that play a role in efficient pricing, it also depends on the nature of businesses and the product that is served.",
+        "status":"",
+        "links":{
+            details:"projects/retail_price_optimization.html",
+            live:"",
+            github:"https://github.com/mohdzain98/Retail_price_optimization"
+        }
+    },
+    {
+        "id" : "csp",
+        "title":"Car Sales Prediction",
+        "area":"Regression project using ML algorithms.",
+        "Description":"Prediction of the car sales using regression model. This project is based on Machine Learning process to predict the sales of cars after training of a model. Based on Regression Model : Multivariate Linear Regression, Support Vector Machines, Random Forest, Adaboost, KNN Regression were used to analyse the best results.",
+        "status":"",
+        "links":{
+            details:"projects/cars_sales_prediction.html",
+            live:"",
+            github:"https://github.com/mohdzain98/Car_Sales_Prediciton"
+        }
+    },
+    {
+        "id" : "sfms",
+        "title":"School Fees Management System",
+        "area":"Database Management System using MySql, HTML, CSS, Python and Flask",
+        "Description":"A School fees management System which is use to manage the student fees which have some functions like adding student, updating fees, deleting student, viewing whole school fees at once.",
+        "status":"",
+        "links":{
+            details:"projects/sfms.html",
+            live:"",
+            github:"https://github.com/mohdzain98/School-Fees-Management-System"
+        }
+    }
+]
+const project = document.getElementById('projects');
+projects.map((element)=>{
+    project.innerHTML = project.innerHTML + `
+    <section id=${element.id}>
+            <div class="box">
+                <center>
+                    <div class="box-header">
+                    <center><h3>${element.title}</h3></center>
+                </div></center>
+                <div class="p2">
+                    <h3 style="color: red; display: inline-block; margin-top: 0px; margin-bottom: 2px;" >Area:</h3> 
+                    <h4 style="display: inline-block; font-weight: lighter; margin-top: 0px;">${element.area}</h4>
+                    <p>${element.Description}</p>
+                    ${element.status.length >1 ?`<h4 style="display: inline-block;">Project is <p style="color:red; display: inline-block;">LIVE </p> on ${element.status}</h4>`:""}
+                </div>
+                ${element.links.details.length>0?`<a href=${element.links.details} class="btn btn-primary">View Details</a>`:""}
+                ${element.links.live.length>0?`<a href=${element.links.live} target="_blank" class="btn btn-primary">View website 
+                <i class="fa fa-external-link" style="color:white;"></i></a>`:""}
+                ${element.links.github.length>0?`<a href=${element.links.github} class="btn btn-primary" style="margin-left:5px" target="_blank">View on Github</a>`:""}
+            </div>
+    </section>
+    `
+})
